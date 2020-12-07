d815fad9f1e8e135d516802f2717a76fb3720969
Mif Masterz <mifmasterz@yahoo.com>
2020-12-07 18:11:37 +0700

    add tester project

--- GHI.PythonEngine/PythonTest/Program.cs
+++ GHI.PythonEngine/PythonTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GHI.PythonSharp;
+using GHIElectronics.TinyCLR.Pins;
+using System;
 using System.Collections;
 using System.Text;
 using System.Threading;
@@ -8,7 +10,12 @@ namespace PythonTest
     class Program
     {
         static void Main()
-        {
+        {   //use UART5 on SITCore SC20260
+
+            //basic testing
+            var module = new PythonSharpEngine(SC20260.UartPort.Uart5);
+            module.Run();
+            Thread.Sleep(Timeout.Infinite);
         }
     }
 }
